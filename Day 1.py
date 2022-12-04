d5645dca87ee79b22b5ad0584efbc4dc6ea52b3c
Ben Cobb <84718179+Cobbcode@users.noreply.github.com>
2022-12-04 14:56:08 +0000

    Day 1 script

---
+++ Day 1.py
@@ -0,0 +1,13 @@
+# Day 1
+import pandas as pd
+import numpy as np
+data = pd.read_csv('Data.csv', header=None,names=["Calories"],skip_blank_lines=False)
+
+list_dfs = np.split(data, data[data.isnull()].index) 
+count_of_calories = sorted(list(map(lambda df: int(df.sum()[0]),df_list)))
+
+# Part 1
+print(f"Part 1 count of calories top elf: {count_of_elfs[-1]}")
+# Part 2
+print(f"Part 2 count of calories top three elfs: {sum(count_of_elfs[-3:])}")
+
